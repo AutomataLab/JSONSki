39346a81174ea008d9aafc8b99394122229a697f
AutomataLab <zhijia@cs.ucr.edu>
2023-09-13 16:51:09 -0700

    Simplify example2.cpp

--- example/example2.cpp
+++ example/example2.cpp
@@ -3,19 +3,14 @@
 
 int main() {
     char* file_path = "../dataset/bestbuy_sample_large_record.json";
-    cout<<"start loading the single large record from "<<file_path<<endl;
     Record* rec = RecordLoader::loadSingleRecord(file_path);
     if (rec == NULL) {
         cout<<"record loading fails."<<endl;
         return -1;
     }
-    cout<<"finish loading the single large record"<<endl;
 
-    string query = "$.products[*].categoryPath[1:3].name";
-    cout<<"\nstart executing query "<<query<<endl;
-    QueryProcessor processor(query);
+    QueryProcessor processor("$.products[*].categoryPath[1:3].name");
     string output = processor.runQuery(rec);
-    cout<<"finish query execution"<<endl;
     cout<<"matches are: "<<output<<endl;
     return 0;
 }

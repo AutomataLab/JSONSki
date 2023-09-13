d0f29d9132640dcbbbdf66d681763647b94025fd
AutomataLab <zhijia@cs.ucr.edu>
2023-09-13 16:49:09 -0700

    Simplify example1.cpp

--- example/example1.cpp
+++ example/example1.cpp
@@ -3,19 +3,14 @@
 
 int main() {
     char* file_path = "../dataset/twitter_sample_large_record.json";
-    cout<<"start loading the single large record from "<<file_path<<endl;
     Record* rec = RecordLoader::loadSingleRecord(file_path);
     if (rec == NULL) {
         cout<<"record loading fails."<<endl;
         return -1;
     }
-    cout<<"finish loading the single large record"<<endl;
 
-    string query = "$[*].entities.urls[*].url";
-    cout<<"\nstart executing query "<<query<<endl;
-    QueryProcessor processor(query);
+    QueryProcessor processor("$[*].entities.urls[*].url");
     string output = processor.runQuery(rec);
-    cout<<"finish query execution"<<endl;
     cout<<"matches are: "<<output<<endl;
     return 0;
 }
